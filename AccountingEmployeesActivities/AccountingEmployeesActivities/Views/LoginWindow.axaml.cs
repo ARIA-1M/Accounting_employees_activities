c69f616625b14dff6b8cd0e2e62bd3b584cdc8d7
ARIA-1M <perevezencevamaria32@gmail.com>
2026-06-15 21:04:23 +0300

    начало работы над авторизацией

---
+++ AccountingEmployeesActivities/AccountingEmployeesActivities/Views/LoginWindow.axaml.cs
@@ -0,0 +1,23 @@
+﻿using AccountingEmployeesActivities.ViewModels;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountingEmployeesActivities.Views
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
